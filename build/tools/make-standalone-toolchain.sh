6a95865d6f49fa28b5f76d39b4d8f24e90f93b67
Andrew Hsieh <andrewhsieh@google.com>
2013-08-28 10:46:01 +0800

    Fix make-standalone-toolchain.sh clang/++ for known archs
    previous commit incorrectly add ndk*-py case in clang/++ for
    known arch
    
    Change-Id: Idede83bccfb5ecd0d07c811fd4e021f6cc1fdcf2

--- build/tools/make-standalone-toolchain.sh
+++ build/tools/make-standalone-toolchain.sh
@@ -418,6 +418,10 @@ if [ "$ARCH_LIB" != "$ARCH" ]; then
     cp -a $NDK_DIR/platforms/$PLATFORM/arch-$ARCH/usr/lib/crt* $TMPDIR/sysroot/usr/lib
 fi
 
+if [ "$ARCH_LIB" != "$ARCH" ]; then
+    cp -a $NDK_DIR/platforms/$PLATFORM/arch-$ARCH/usr/lib/crt* $TMPDIR/sysroot/usr/lib
+fi
+
 dump "Copying libstdc++ headers and libraries..."
 
 GNUSTL_DIR=$NDK_DIR/$GNUSTL_SUBDIR/$GCC_VERSION
